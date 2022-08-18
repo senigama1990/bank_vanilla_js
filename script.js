274d49e14e6c1db773fd51f59b2a803620b2ba5d
senigama1990 <senigama1990@mail.ru>
2022-08-18 17:30:39 +0500

    smoothy scroll and interactive tabs

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modalWindow = document.querySelector('.modal-window');
 const overlay = document.querySelector('.overlay');
 const btnCloseModalWindow = document.querySelector('.btn--close-modal-window');
 const btnsOpenModalWindow = document.querySelectorAll(
   '.btn--show-modal-window'
 );
+const btnScrollTo = document.querySelector(".btn--scroll-to")
+const section1 = document.querySelector("#section--1")
+const tabs = document.querySelectorAll('.operations__tab')
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabContents = document.querySelectorAll('.operations__content')
 
+
+///////////////////////////////////////
+// Modal window
 const openModalWindow = function (e) {
   e.preventDefault()
   modalWindow.classList.remove('hidden');
@@ -31,3 +36,49 @@ document.addEventListener('keydown', function (e) {
     closeModalWindow();
   }
 });
+
+
+// smooth scroll to section-1 from header
+
+
+btnScrollTo.addEventListener('click', function (e) {
+  // const section1Coords = section1.getBoundingClientRect();
+  // window.scrollTo({
+  //   left: section1Coords.left + window.pageXOffset,
+  //   top: section1Coords.top + window.pageYOffset,
+  //   behavior: "smooth"
+  // })
+
+  section1.scrollIntoView({behavior: "smooth"})
+})
+
+// document.querySelectorAll(".nav__link").forEach(HTMLElement => {
+//   HTMLElement.addEventListener("click", function (e) {
+//     e.preventDefault()
+//     const href = this.getAttribute('href')
+//     document.querySelector(href).scrollIntoView({behavior: "smooth"})
+//   })
+// })
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault()
+  if (e.target.classList.contains('nav__link')) {
+    const href = e.target.getAttribute('href')
+    document.querySelector(href).scrollIntoView({behavior: "smooth"})
+  }
+})
+
+// operations__tab
+
+tabsContainer.addEventListener('click', function (e) {
+  const clickedButton = e.target.closest('.operations__tab')
+  // guard clause
+  if (!clickedButton) return
+  // active tab
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'))
+  clickedButton.classList.add('operations__tab--active')
+  // avtive content
+  tabContents.forEach(content => content.classList.remove('operations__content--active'))
+  
+  document.querySelector(`.operations__content--${clickedButton.dataset.tab}`).classList.add('operations__content--active')
+})
